aa741c3c92d1ce402333780f0b9eb5b532ce5e12
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-10-29 19:55:29 -0300

    modified:   game.py 	fazendo projeto inicial do board
    	modified:   main.py

--- game.py
+++ game.py
@@ -3,6 +3,20 @@ class Game:
         self.height = height
         self.width = width
 
-    def render(self):
-        ...
+    def board_matrix(self):
+        board = [
+            ['+', '-', '-', '+'],
+            ['|', ' ', ' ', '|'],
+            ['|', ' ', ' ', '|'],
+            ['|', ' ', ' ', '|'],
+            ['|', ' ', ' ', '|'],
+            ['|', ' ', ' ', '|'],
+            ['+', '-', '-', '+'],
+        ]
+        return board 
 
+    def render(self):
+        matrix = self.board_matrix()
+
+        for row in matrix:
+            print(*row)
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from game import Game
 
 if __name__ == '__main__':
     snake_game = Game(10,50)
-
+    snake_game.render()
